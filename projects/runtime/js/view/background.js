5280587154f654259dccd2e66dca95bd42a55a40
JawanMcdonald <jawan.mcdonald@livingstoncollegiate.org>
2019-11-07 20:18:02 +0000

    <runtime update>

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -26,6 +26,7 @@ var background = function (window) {
         // container which will be returned
         var background;
         var tree;
+        var buildings = [];
         // ANIMATION VARIABLES HERE:
         
      
@@ -59,10 +60,21 @@ moon.scaleY = .70;
 background.addChild(moon);
             
             // TODO: 5 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
-            
+var buildingHeight = 300;
+var building;
+for(var i=0;i<10;++i) {
+    building = draw.rect(75,buildingHeight,'black','yellow',1);
+    building.x = 200*i;
+    building.y = groundY-buildingHeight;
+    background.addChild(building);
+    buildings.push(building);
+}
             
             // TODO 4: Part 1 - Add a tree
-            
+            tree = draw.bitmap('img/tree.png');
+tree.x = 1600;
+tree.y = 250;
+background.addChild(tree);
             
         } // end of render function - DO NOT DELETE
         
@@ -76,13 +88,16 @@ background.addChild(moon);
             var groundY = ground.y;
             
             // TODO 4: Part 2 - Move the tree!
-            tree = draw.bitmap('img/tree.png');
-tree.x = 0;
-tree.y = 0;
-background.addChild(tree);
+            tree.x = tree.x + -4;
+            if(tree.x < -200) {
+    tree.x = canvasWidth;
+}
             
             // TODO 5: Part 2 - Parallax
-            
+            building.x = building.x + -1;
+            if(building.x < -200) {
+    building.x = canvasWidth;
+}
 
         } // end of update function - DO NOT DELETE
         
