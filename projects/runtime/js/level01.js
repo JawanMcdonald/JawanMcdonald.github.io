5896c89153e54cd5d1e554407bbb542171351948
JawanMcdonald <jawan.mcdonald@livingstoncollegiate.org>
2019-11-22 02:05:41 +0000

    <RuntimeCompletion>

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -17,7 +17,13 @@ var level01 = function (window) {
             gameItems: [
                 {type: 'sawblade',x:400,y:375},
                 {type: 'sawblade',x:600,y:470},
-                {type: 'sawblade',x:1200,y:470}
+                {type: 'sawblade',x:1200,y:470},
+                {type: 'enemy',x:400,y:-10},
+                {type: 'enemy',x:800,y:-100},
+                {type: 'enemy',x:1200,y:-50},
+                {type: 'reward',x:400,y:-10},
+                {type: 'reward',x:600,y:470},
+                {type: 'reward',x:530,y:330}
             ]
         };
         window.levelData = levelData;
@@ -39,6 +45,7 @@ function createSawBlade(x,y) {
     obstacleImage.x = -25;
     obstacleImage.y = -25;
     }
+    function createEnemy(x, y) {
     var enemy =  game.createGameItem('enemy',25);
 var redSquare = draw.rect(50,50,'red');
 redSquare.x = -25;
@@ -47,11 +54,39 @@ enemy.addChild(redSquare);
 enemy.x = 400;
 enemy.y = groundY-50;
 enemy.velocityX = -1;
+enemy.rotationalVelocity =10;
 game.addGameItem(enemy);
-    
-    
-    
-    
+
+enemy.onPlayerCollision = function() {
+    console.log('The enemy has hit Halle');
+    game.changeIntegrity(-10);
+    enemy.fadeOut();
+};
+ enemy.onProjectileCollision = function() {
+     console.log('Hallebot has hit the enemy!');
+     game.increaseScore(100);
+     enemy.fadeOut();
+ };    
+}
+
+function createReward(x,y){
+var reward = game.createGameItem('reward',25);
+var rewardImage = draw.rect(50,50,'green');
+rewardImage.x = -25;
+rewardImage.y = -25;
+reward.addChild(rewardImage);
+reward.x = 400;
+reward.y = groundY-50;
+reward.velocityX = -1;
+game.addGameItem(reward);
+
+reward.onPlayerCollision = function() {
+  console.log('Hallebot got points!');
+  game.increaseScore(50);
+  reward.fadeOut();
+};    
+}
+
     
     
     
@@ -66,12 +101,12 @@ game.addGameItem(enemy);
         if (levelData.gameItems[j].type === 'sawblade'){
             createSawBlade(gameItem.x, gameItem.y);
             }
-            if (leveldata.gameItems[j].type === 'enemy'){
+            if (levelData.gameItems[j].type === 'enemy'){
                createEnemy(gameItem.x, gameItem.y);
             }
-//            if (levelData.gameItems[j].type === 'reward'){
-//                createReward(gameItem.x, gameItem.y);
-//            }
+            if (levelData.gameItems[j].type === 'reward'){
+                createReward(gameItem.x, gameItem.y);
+            }
     }
 
    };
